894a308bb739738a510cbf5effd8fc994e195b51
Clemens Lange <clemens.lange@cern.ch>
2018-02-21 13:26:59 +0100

    add some more commented out submission options

--- resolutionScaleFiller_submit.sh
+++ resolutionScaleFiller_submit.sh
@@ -2,13 +2,14 @@
 
 SAMPLEFILE=$1
 
-OUTDIR="/eos/user/c/clange/HGCal/ScaleResolution/skip_24_11_11/"
+OUTDIR="/eos/user/c/clange/HGCal/ScaleResolution/out/"
 PREFIX="root://eoscms.cern.ch//eos/cms/store/cmst3/group/hgcal/CMG_studies/Production/"
 GUNTYPE="pt"
 POSTFIX="NTUP"
 REFNAME="genpart"
 # OBJNAMES="pfcluster megacluster"
-OBJNAMES="megacluster"
+# OBJNAMES="megacluster"
+OBJNAMES="pfcluster_manual"
 QUEUE="8nh"
 
 for SAMPLE in `cat $SAMPLEFILE`; do
@@ -23,9 +24,13 @@ for SAMPLE in `cat $SAMPLEFILE`; do
     PID=22
   fi;
   for OBJNAME in $OBJNAMES; do
-    echo python resolutionScaleFiller_batchWrapper.py --inputdir ${SAMPLEDIR} --outdir ${OUTDIR} --gunType $GUNTYPE --pid $PID --genValue $PTVAL --tag $TAG --ref $REFNAME --obj $OBJNAME --queue $QUEUE --skipLayers 3,10,17,24,34,46
+    echo python resolutionScaleFiller_batchWrapper.py --inputdir ${SAMPLEDIR} --outdir ${OUTDIR} --gunType $GUNTYPE --pid $PID --genValue $PTVAL --tag $TAG --ref $REFNAME --obj $OBJNAME --queue $QUEUE
+     # --skipLayers 30,32,34,36,38,40
+    # --skipLayers 3,10,17,24,34,46
     # --skipLayers 2,4,7,10,13,16,19,22,25,27,30,34,38,42,46,50
-    python resolutionScaleFiller_batchWrapper.py --inputdir ${SAMPLEDIR} --outdir ${OUTDIR} --gunType $GUNTYPE --pid $PID --genValue $PTVAL --tag $TAG --ref $REFNAME --obj $OBJNAME --queue $QUEUE --skipLayers 3,10,17,24,34,46
+    python resolutionScaleFiller_batchWrapper.py --inputdir ${SAMPLEDIR} --outdir ${OUTDIR} --gunType $GUNTYPE --pid $PID --genValue $PTVAL --tag $TAG --ref $REFNAME --obj $OBJNAME --queue $QUEUE
+    # --skipLayers 30,32,34,36,38,40
+    # --skipLayers 3,10,17,24,34,46
     # --skipLayers 2,4,7,10,13,16,19,22,25,27,30,34,38,42,46,50
   done
 done
